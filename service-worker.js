4fcb612511b63e181a2a750c08d534ea63676fa0
siamnano95-web <siamnano95@gmail.com>
2026-05-04 05:58:04 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
-self.addEventListener('install', e => self.skipWaiting());
+const CACHE_NAME = "thai-wcode-cache-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
